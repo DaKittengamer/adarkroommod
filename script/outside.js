df3deeffc5659cad834d2868a03260c2369871d9
DaKittengamer <kittengamer002@gmail.com>
2026-02-18 13:28:55 -0600

    Update outside.js

--- script/outside.js
+++ script/outside.js
@@ -176,9 +176,7 @@ var Outside = {
 	},
 	
 	getMaxPopulation: function() {
-		var a = $SM.get('game.buildings["hut"]', true) * Outside._HUT_ROOM;
-		var b = $SM.get('game.buildings["house"]', true) * Outside._HOUSE_ROOM;
-		return a + b;
+		return ($SM.get('game.buildings["hut"]', true) * Outside._HUT_ROOM) + ($SM.get('game.buildings["house"]', true) * Outside._HOUSE_ROOM;)	
 	},
 	
 	increasePopulation: function() {
